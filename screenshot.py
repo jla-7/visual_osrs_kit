08bc80b73dc4462094bcb482c7e55829bc2ed5aa
Jake Adams <jake.luca@gmail.com>
2025-07-23 20:05:13 +0100

    Adjust window size

--- screenshot.py
+++ screenshot.py
@@ -1,16 +1,17 @@
 import pygetwindow as gw
 
-def getClient():
-    resizeX = 949
-    resizeY = 619
+def get_client():
+    resize_x = 936
+    resize_y = 611
 
     for title in gw.getAllTitles():
         if title != "":
             print(title)
         if title == 'Old School RuneScape':
             osrs = gw.getWindowsWithTitle('Old School RuneScape')[0]
-            osrs.resizeTo(resizeX,resizeY)
+            osrs.resizeTo(resize_x,resize_y)
             osrs.activate()
+            osrs.moveTo(10,10)
             break
 
-getClient()
+get_client()
